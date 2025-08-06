cd4c7a7c46e5056b9cec8a0022372321b3b4c41e
Muralidhar Chavan <muralidhar.chavan@gmail.com>
2025-08-06 10:56:30 +0530

    add db router

--- backend/main.py
+++ backend/main.py
@@ -5,3 +5,7 @@ from routers import db_router
 app = FastAPI()
 app.include_router(score_answers.router)
 app.include_router(db_router.router)
+
+@app.get("/")
+def read_root():
+    return {"status": "ok", "message": "Anywai UDAO backend app is up and running"}
